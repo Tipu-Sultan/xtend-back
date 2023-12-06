bad626fad4786155ad5395b69e29be707d50cd4e
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-12-06 16:35:02 +0530

    fix bugs

--- routes/upload.js
+++ routes/upload.js
@@ -1,55 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const mammoth = require('mammoth')
-const Question = require('../models/question')
+const Question = require('../models/question');
+const WordExtractor = require('word-extractor');
+
+const extractor = new WordExtractor();
 
 // Set up multer for file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post('/api/upload', upload.single('file'), async function (req, res, next) {
-    try {
-        const result = await mammoth.extractRawText({ buffer: req.file.buffer });
-        const text = result.value;
+router.post('/api/upload', upload.single('file'), async function (req, res) {
+  try {
+    const docxBuffer = req.file.buffer;
+    const extracted = extractor.extract(docxBuffer);
 
-        const questionsData = text.split(/\n{3,}/);
+    extracted.then(async (docx) => {
+      const text = docx.getBody();
+      const questionsData = text.split(/\n\n/); // Adjust the splitting logic
 
-        for (let index = 0; index < questionsData.length; index++) {
-            const questionData = questionsData[index].trim();
+      console.log(questionsData);
+      console.log(questionsData.length);
 
-            if (!questionData) {
-                continue;
-            }
+      for (let index = 0; index < questionsData.length; index++) {
+        const questionData = questionsData[index].trim();
 
-            // Use regular expressions to extract question and options
-            const questionMatch = questionData.match(/Q\d+\.\s*([\s\S]*?)(\n|$)/);
-            const optionsMatches = questionData.match(/[A-D]\)\s*([\s\S]*?)(?=\n[A-D]|\nAnswer:|$)/g);
-            const correctAnswerMatch = questionData.match(/Answer:\s*([A-D]\))/);
-
-
-            if (questionMatch && optionsMatches && correctAnswerMatch) {
-                const questionText = questionMatch[1].trim();
-                const options = optionsMatches.map((match) => match.trim());
-                const correctAnswer = correctAnswerMatch[1].trim();
-
-                const newQuestion = new Question({
-                    question: questionText,
-                    options: options,
-                    correctAnswer: correctAnswer,
-                });
-
-                // Save the question to the database
-                await newQuestion.save();
-            }
+        if (!questionData) {
+          continue;
         }
 
-        return res.status(200).json({ data: text, msg: 'Questions uploaded successfully.' });
-    } catch (error) {
-        console.error('Error uploading questions:', error);
-        return res.status(500).send('Internal Server Error');
-    }
-});
+        // Use regular expressions to extract question and options
+        const questionMatch = questionData.match(/Q\d+\.\s*([\s\S]*?)(\n|$)/);
+        const optionsMatches = questionData.match(/[A-D]\)\s*([\s\S]*?)(?=\n[A-D]|\nAnswer:|$)/g);
+        const correctAnswerMatch = questionData.match(/Answer:\s*([A-D]\))/);
 
+        if (questionMatch && optionsMatches && correctAnswerMatch) {
+          const questionText = questionMatch[1].trim();
+          const options = optionsMatches.map((match) => match.trim());
+          const correctAnswer = correctAnswerMatch[1].trim();
+
+          const newQuestion = new Question({
+            question: questionText,
+            options: options,
+            correctAnswer: correctAnswer,
+          });
+          await newQuestion.save();
+        }
+      }
+
+      return res.status(200).json({ data: questionsData, msg: 'Questions uploaded successfully.' });
+    }).catch((err) => {
+      console.log('Error extracting content from Word document:', err);
+      return res.status(500).send('Internal Server Error');
+    });
+  } catch (error) {
+    console.error('Error uploading questions:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
